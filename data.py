e5b07b393bd9bc92cfbc7d5951b4f3278469a571
Asad-Rizwan <106770564+Asad-Rizwan@users.noreply.github.com>
2023-06-11 18:54:25 +0500

    Update data.py

--- data.py
+++ data.py
@@ -9,15 +9,24 @@ transform = transforms.Compose([
 ])
 
 # Load the CIFAR-10 training dataset
-trainset = torchvision.datasets.CIFAR10(root='./data', 
-                                        train=True, 
-                                        download=False, 
+trainset = torchvision.datasets.CIFAR10(root='./data',
+                                        train=True,
+                                        download=False,
                                         transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=4,
+                                          shuffle=True,
+                                          num_workers=2)
 
 # Load the CIFAR-10 test dataset
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+testset = torchvision.datasets.CIFAR10(root='./data',
+                                       train=False,
+                                       download=False,
+                                       transform=transform)
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=4,
+                                         shuffle=False,
+                                         num_workers=2)
 
 # Define the classes for CIFAR-10
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
